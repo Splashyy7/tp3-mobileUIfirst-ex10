d196cff01903d62b245658af51a1887c9b9faed4
João Pedro Oliveira Gritz de Oliveira <135381904+Splashyy7@users.noreply.github.com>
2024-09-19 12:20:12 -0300

    first commit

---
+++ src/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Content.module.css';
+
+const Content = () => {
+  return (
+    <main className={styles.content}>
+      <p>Content</p>
+    </main>
+  );
+};
+
+export default Content;
